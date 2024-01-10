8e208817c8c50373ce374c8f351c626dc9922bf6
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-01-10 09:32:08 -0500

    Implement new API that only exposes necessary quanitites to clients

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 export { createShip };
 
-function createShip(length) {
-  if (length < 1)
-    throw new Error(`Cannot create a ship with length (${length}) less than 1`);
+function createShip(length, name) {
+  // Error checking
+  if (length < 1) {
+    throw new Error(
+      `Cannot create a ship with length (${length}) less than 1!`
+    );
+  }
+
+  // Private variables
+  const _length = length;
+  const _name = name;
+  let _hitCount = 0;
+
+  // Increments hit count on ship if it is not sunk. Errors if ship is sunk.
+  const hit = function () {
+    if (isSunk()) {
+      throw new Error("Cannot hit a ship that is already sunk!");
+    }
+    _hitCount += 1;
+  };
+
+  // Returns true if ship is sunk, false otherwise
+  const isSunk = function () {
+    return _hitCount == _length;
+  };
+
+  // Returns the name of the ship
+  const getName = function () {
+    return _name;
+  };
+
   return {
-    length,
-    numHits: 0,
-    hit() {
-      this.numHits += this.isSunk() ? 0 : 1;
-    },
-    isSunk() {
-      return this.numHits >= this.length;
-    },
+    hit,
+    isSunk,
+    getName,
   };
 }
