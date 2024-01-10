36f7a4ee11e748dcbf89318b124f5e329290c6ee
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-01-10 12:22:04 -0500

    Implement createGameboard factory with ship placing logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export { createShip, createGameboard };
 
-function createShip(length, name) {
+function createShip(name, length) {
   // Error checking
   if (length < 1) {
     throw new Error(
@@ -39,19 +39,103 @@ function createShip(length, name) {
 }
 
 function createGameboard() {
-  const placeShip = function (name, length, row, col, orientation) {};
-  const receiveAttack = function (row, col) {};
-  const attackStatus = function (row, col) {};
-  const attackAllowed = function (row, col) {};
-  const hasShip = function (row, col) {};
+  const BOARD_SIZE = 10;
+
+  function getBoardWithFill(fillValue) {
+    return [...Array(BOARD_SIZE)].map((e) => Array(BOARD_SIZE).fill(fillValue));
+  }
+
+  const _ships = getBoardWithFill(null);
+  const _attacks = getBoardWithFill(null);
+  let _numShipsLeft = 0;
+
+  function validateRowCol(row, col) {
+    if (row < 0 || col < 0 || row >= BOARD_SIZE || col >= BOARD_SIZE) {
+      throw new Error(
+        `Invalid (row, col) pair (${row}, ${col}) for board dimensions!`
+      );
+    }
+  }
+
+  function validateShipPlacement(length, row, col, orientation) {
+    // Check for violations of board boundaries
+    validateRowCol(row, col);
+    if (orientation !== "h" && orientation !== "v") {
+      throw new Error(
+        `Orientation must be 'h' or 'v'! Value provided: ${orientation}`
+      );
+    }
+    if (orientation === "h" && col + length > BOARD_SIZE) {
+      throw new Error(
+        `Horizontally placing ship of length ${length} at row ${row}, col ${col} would violate board bounds!`
+      );
+    }
+    if (orientation === "v" && row + length > BOARD_SIZE) {
+      throw new Error(
+        `Vertically placing ship of length ${length} at row ${row}, col ${col} would violate board bounds!`
+      );
+    }
+    // Check if intersects exiting ships
+    const shipLengthRange = [...Array(length).keys()];
+    for (let delta of shipLengthRange) {
+      const intersecting =
+        orientation === "h"
+          ? hasShip(row, col + delta)
+          : hasShip(row + delta, col);
+      if (intersecting) {
+        throw new Error(
+          `Ship placement intersects a ship that is already on the board!`
+        );
+      }
+    }
+  }
+
+  const placeShip = function (name, length, row, col, orientation) {
+    validateShipPlacement(length, row, col, orientation);
+
+    const ship = createShip(name, length);
+    for (let delta = 0; delta < length; delta++) {
+      if (orientation == "h") {
+        _ships[row][col + delta] = ship;
+      } else {
+        _ships[row + delta][col] = ship;
+      }
+    }
+    _numShipsLeft += 1;
+  };
+
+  const hasShip = function (row, col) {
+    validateRowCol(row, col);
+    return _ships[row][col] ? true : false;
+  };
+
   const allShipsSunk = function () {};
+
+  const receiveAttack = function (row, col) {
+    validateRowCol(row, col);
+    if (!attackAllowed(row, col)) {
+      throw new Error(`Square at (${row}, ${col}) has already been attacked!`);
+    }
+    const hitShip = hasShip(row, col);
+    _attacks[row][col] = hitShip ? true : false;
+    if (hitShip) _ships[row][col].hit();
+  };
+
+  const attackStatus = function (row, col) {
+    return _attacks[row][col];
+  };
+
+  const attackAllowed = function (row, col) {
+    validateRowCol(row, col);
+    return attackStatus(row, col) === null;
+  };
 
   return {
     placeShip,
+    hasShip,
+    allShipsSunk,
     receiveAttack,
     attackStatus,
     attackAllowed,
-    hasShip,
-    allShipsSunk,
   };
 }
