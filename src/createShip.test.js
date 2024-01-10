130984fd18a2d1aef17ab22b8b02bc8a9cab4279
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-01-10 09:32:23 -0500

    Rewrite tests for updated API

--- src/createShip.test.js
+++ src/createShip.test.js
@@ -1,40 +1,41 @@
-import { createShip } from "./index.js";
+import { createShip } from ".";
 
-test("creating ship error detection", () => {
-  expect(() => createShip(1)).not.toThrow();
-  expect(() => createShip(0)).toThrow(
-    `Cannot create a ship with length (0) less than 1`
+test("creating ship length error detection", () => {
+  expect(() => createShip(1, "one")).not.toThrow();
+  expect(() => createShip(0, "zero")).toThrow(
+    `Cannot create a ship with length (0) less than 1!`
   );
-  expect(() => createShip(-1)).toThrow(
-    `Cannot create a ship with length (-1) less than 1`
+  expect(() => createShip(-1, "negative one")).toThrow(
+    `Cannot create a ship with length (-1) less than 1!`
   );
-  expect(() => createShip(100)).not.toThrow();
+  expect(() => createShip(100, "one hundred")).not.toThrow();
 });
 
-test("ship length set properly", () => {
-  expect(createShip(1).length).toBe(1);
-  expect(createShip(2).length).toBe(2);
-  expect(createShip(100).length).toBe(100);
+test("getName() returns the proper ship name", () => {
+  expect(createShip(1, "one").getName()).toBe("one");
+  expect(createShip(2, "two").getName()).toBe("two");
+  expect(createShip(100, "one hundered").getName()).toBe("one hundered");
 });
 
-test("ship hit function increments numHits and sinks ship", () => {
+test("Ship is not sunk until hit is called length times", () => {
   const length = 5;
-  const ship = createShip(length);
+  const ship = createShip(length, "five");
   for (let i = 0; i < length; i++) {
-    expect(ship.numHits).toBe(i);
     expect(ship.isSunk()).toBe(false);
     ship.hit();
   }
-  expect(ship.numHits).toBe(length);
   expect(ship.isSunk()).toBe(true);
 });
 
-test("Ship numHits never exceeds length", () => {
-  for (let length = 1; length < 11; length++) {
-    const ship = createShip(length);
-    for (let hits = 0; hits < 2 * length; hits++) {
-      ship.hit();
-      expect(ship.numHits).toBeLessThanOrEqual(length);
-    }
+test("Calling hit on a sunken ship throws error and does not alter sunken state", () => {
+  const length = 5;
+  const ship = createShip(length, "five");
+  for (let hits = 0; hits < length; hits++) {
+    expect(ship.isSunk()).toBe(false);
+    expect(() => ship.hit()).not.toThrow();
   }
+  expect(ship.isSunk()).toBe(true);
+  // Hit it more than length times
+  expect(() => ship.hit()).toThrow();
+  expect(ship.isSunk()).toBe(true);
 });
