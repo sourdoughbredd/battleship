1f0669a380fea538eb6daf07d43bf8e3588a296d
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-01-10 12:21:18 -0500

    Update to refelct new inptu argument order

--- src/createShip.test.js
+++ src/createShip.test.js
@@ -1,25 +1,25 @@
 import { createShip } from ".";
 
 test("creating ship length error detection", () => {
-  expect(() => createShip(1, "one")).not.toThrow();
-  expect(() => createShip(0, "zero")).toThrow(
+  expect(() => createShip("one", 1)).not.toThrow();
+  expect(() => createShip("zero", 0)).toThrow(
     `Cannot create a ship with length (0) less than 1!`
   );
-  expect(() => createShip(-1, "negative one")).toThrow(
+  expect(() => createShip("negative one", -1)).toThrow(
     `Cannot create a ship with length (-1) less than 1!`
   );
-  expect(() => createShip(100, "one hundred")).not.toThrow();
+  expect(() => createShip("one hundred", 100)).not.toThrow();
 });
 
 test("getName() returns the proper ship name", () => {
-  expect(createShip(1, "one").getName()).toBe("one");
-  expect(createShip(2, "two").getName()).toBe("two");
-  expect(createShip(100, "one hundered").getName()).toBe("one hundered");
+  expect(createShip("one", 1).getName()).toBe("one");
+  expect(createShip("two", 2).getName()).toBe("two");
+  expect(createShip("one hundred", 100).getName()).toBe("one hundred");
 });
 
 test("Ship is not sunk until hit is called length times", () => {
   const length = 5;
-  const ship = createShip(length, "five");
+  const ship = createShip("five", length);
   for (let i = 0; i < length; i++) {
     expect(ship.isSunk()).toBe(false);
     ship.hit();
@@ -29,7 +29,7 @@ test("Ship is not sunk until hit is called length times", () => {
 
 test("Calling hit on a sunken ship throws error and does not alter sunken state", () => {
   const length = 5;
-  const ship = createShip(length, "five");
+  const ship = createShip("five", length);
   for (let hits = 0; hits < length; hits++) {
     expect(ship.isSunk()).toBe(false);
     expect(() => ship.hit()).not.toThrow();
