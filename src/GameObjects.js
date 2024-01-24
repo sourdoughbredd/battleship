a2b0461d6d5bc9ac8b0a8cc54c8a054ed8ea2359
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-01-24 15:31:53 -0500

    Implement medium difficulty AI

--- src/GameObjects.js
+++ src/GameObjects.js
@@ -149,6 +149,7 @@ function createGameboard(boardName) {
       _ships[row][col].hit();
       _numShipsLeft -= _ships[row][col].isSunk();
     }
+    return hitShip;
   };
 
   const attackStatus = function (row, col) {
@@ -156,7 +157,11 @@ function createGameboard(boardName) {
   };
 
   const attackAllowed = function (row, col) {
-    validateRowCol(row, col);
+    try {
+      validateRowCol(row, col);
+    } catch {
+      return false;
+    }
     return attackStatus(row, col) === null;
   };
 
@@ -227,12 +232,200 @@ function createComputerPlayer(board, opponentBoard) {
 
   // Attack a random spot from the set of all allowable spots
   computer.attack = function () {
-    const attackableSpots = opponentBoard.attackableSpots();
-    const [rowToAttack, colToAttack] =
-      attackableSpots[getRandomInt(0, attackableSpots.length)];
-    // console.log(`computer attacking ${rowToAttack}, ${colToAttack}`);
-    opponentBoard.receiveAttack(rowToAttack, colToAttack);
+    computerAttackLogicMedium();
   };
 
+  function computerAttackLogicEasy() {
+    const [row, col] = getRandomAttackableSpot(opponentBoard);
+    opponentBoard.receiveAttack(row, col);
+  }
+
+  // Variables that hard-level AI needs to maintain throughout game
+  let mode = "LOCATING"; // LOCATING, ORIENTING, DESTROYING
+
+  const attackHistory = {};
+  resetAttackHistory();
+
+  function resetAttackHistory() {
+    attackHistory.lastAttack = { row: null, col: null };
+    // attackHistory.lastAttackHit = false;
+    attackHistory.initialHit = { row: null, col: null };
+    attackHistory.orientation = null; // "h" or "v"
+    attackHistory.direction = null; // +/- 1
+    attackHistory.numDirectionsFinished = 0;
+  }
+
+  function computerAttackLogicMedium() {
+    let row, col;
+    switch (mode) {
+      case "LOCATING": {
+        locatingLogic();
+        break;
+      }
+      case "ORIENTING": {
+        // Last attack was a hit. Now trying to determine orientation of ship
+        orientingLogic();
+        break;
+      }
+      case "DESTROYING": {
+        // Orientation determined. Destroy ship in one direction, then the other!
+        destroyingLogic();
+        break;
+      }
+      default:
+        break;
+    }
+    console.log(attackHistory);
+  }
+
+  // LOCATING logic
+  function locatingLogic() {
+    // Choose a random spot
+    const [row, col] = getRandomAttackableSpot(opponentBoard);
+    const hit = opponentBoard.receiveAttack(row, col);
+    // Update attack history
+    attackHistory.lastAttack.row = row;
+    attackHistory.lastAttack.col = col;
+    // Change states if attack was a hit
+    if (hit) {
+      attackHistory.initialHit = { row, col };
+      console.log("initial hit is");
+      console.log(attackHistory.initialHit);
+      mode = "ORIENTING";
+    }
+  }
+
+  function orientingLogic() {
+    // Attack a random neighbor of initial hit location
+    const [row, col] = getRandomAttackableNeighbor(
+      opponentBoard,
+      attackHistory.initialHit.row,
+      attackHistory.initialHit.col
+    );
+    const hit = opponentBoard.receiveAttack(row, col);
+    // Update attack history
+    attackHistory.lastAttack.row = row;
+    attackHistory.lastAttack.col = col;
+    // Change states if attack was a hit
+    if (hit) {
+      mode = "DESTROYING";
+      attackHistory.orientation =
+        row == attackHistory.initialHit.row ? "h" : "v";
+      attackHistory.direction =
+        attackHistory.orientation === "h"
+          ? Math.sign(col - attackHistory.initialHit.col)
+          : Math.sign(row - attackHistory.initialHit.row);
+      // Look around to see if we are finished with any directions yet
+      const [nextRow, nextCol] = getNextDirectedNeighbor(
+        attackHistory.lastAttack.row,
+        attackHistory.lastAttack.col,
+        attackHistory.orientation,
+        attackHistory.direction
+      );
+      const [oppoRow, oppoCol] = getNextDirectedNeighbor(
+        attackHistory.initialHit.row,
+        attackHistory.initialHit.col,
+        attackHistory.orientation,
+        attackHistory.direction * -1
+      );
+      if (!opponentBoard.attackAllowed(nextRow, nextCol)) {
+        attackHistory.numDirectionsFinished += 1;
+        attackHistory.direction *= -1;
+      }
+      if (!opponentBoard.attackAllowed(oppoRow, oppoCol)) {
+        attackHistory.numDirectionsFinished += 1;
+      }
+      if (attackHistory.numDirectionsFinished === 2) {
+        mode = "ORIENTING";
+        resetAttackHistory();
+        return;
+      }
+    }
+  }
+
+  function destroyingLogic() {
+    // Get next attack location
+    const [row, col] = getNextDirectedNeighbor(
+      attackHistory.lastAttack.row,
+      attackHistory.lastAttack.col,
+      attackHistory.orientation,
+      attackHistory.direction
+    );
+
+    // If attack is not allowed, need to increment number of finished directions
+    if (!opponentBoard.attackAllowed(row, col)) {
+      // We finished this direction
+      attackHistory.numDirectionsFinished += 1;
+      if (attackHistory.numDirectionsFinished === 1) {
+        // Still need to go other direction.
+        attackHistory.lastAttack.row = attackHistory.initialHit.row;
+        attackHistory.lastAttack.col = attackHistory.initialHit.col;
+        attackHistory.direction *= -1;
+      } else if (attackHistory.numDirectionsFinished === 2) {
+        // Finished both directions
+        mode = "LOCATING";
+        resetAttackHistory();
+        locatingLogic();
+        return;
+      }
+    }
+    // Attack
+    const hit = opponentBoard.receiveAttack(row, col);
+    // Update attack history
+    attackHistory.lastAttack.row = row;
+    attackHistory.lastAttack.col = col;
+
+    // Look ahead to next turn to see if we need to switch our direction.
+    // If this turn wasn't a hit or next neighbor (next turn) not attackable, we finished in this direction
+    const [nextRow, nextCol] = getNextDirectedNeighbor(
+      row,
+      col,
+      attackHistory.orientation,
+      attackHistory.direction
+    );
+    if (!hit || !opponentBoard.attackAllowed(nextRow, nextCol)) {
+      // We finished this direction
+      attackHistory.numDirectionsFinished += 1;
+      if (attackHistory.numDirectionsFinished === 1) {
+        // Still need to go other direction.
+        attackHistory.lastAttack.row = attackHistory.initialHit.row;
+        attackHistory.lastAttack.col = attackHistory.initialHit.col;
+        attackHistory.direction *= -1;
+      } else if (attackHistory.numDirectionsFinished === 2) {
+        // Finished both directions
+        mode = "LOCATING";
+        resetAttackHistory();
+      }
+    }
+  }
+
   return computer;
+}
+
+function getRandomAttackableSpot(board) {
+  return getRandomArrayEntry(board.attackableSpots());
+}
+
+function getRandomArrayEntry(array) {
+  return array[getRandomInt(0, array.length)];
+}
+
+function getRandomAttackableNeighbor(board, row, col) {
+  const attackableNeighbors = board.attackableSpots().filter(([aRow, aCol]) => {
+    return (
+      (oneAway(row, aRow) && col === aCol) ||
+      (row === aRow && oneAway(col, aCol))
+    );
+  });
+  // console.log("attackable neighbors: ");
+  // console.log(attackableNeighbors);
+  return getRandomArrayEntry(attackableNeighbors);
+}
+
+function getNextDirectedNeighbor(row, col, orientation, direction) {
+  return orientation == "h" ? [row, col + direction] : [row + direction, col];
+}
+
+function oneAway(a, b) {
+  return Math.abs(a - b) === 1;
 }
