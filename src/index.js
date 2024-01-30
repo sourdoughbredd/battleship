e7a3699f7c256e8effd6583e1f7e13381a0bff61
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-01-30 09:55:57 -0500

    Create game with user selected difficulty

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { createGame, createReplay } from "./game.js";
 import { UI } from "./ui.js";
 
 // INITIALIZE UI
-const home = UI.createHomePage();
-await home.solicitStartGame();
+const home = UI.loadHomePage();
+const difficulty = await home.solicitStartGame();
 
 // NEW GAME
-const game = createGame();
+const game = createGame(difficulty);
 await game.startGame();
 
 // RESTIM
